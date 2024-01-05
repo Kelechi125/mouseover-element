595d906bd1a2ec1d932335d58036ae2307d00255
Kelechi125 <kelechi125@yahoo.com>
2024-01-04 20:16:24 -0600

    Declare "tx" and "ty" variables and store them inside canvas.width and canvas.height variables

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log("Hello World!");
 
 const canvas = document.getElementById("canvas");
 const c = canvas.getContext("2d");
+const tx = window.innerWidth;
+const ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
