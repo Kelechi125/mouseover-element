8fa059bc02fd49df25e89fc57261f481dbaa9034
Kelechi125 <kelechi125@yahoo.com>
2024-01-06 08:10:55 -0600

    Create Ball function; create bal array; create a for loop that pushes elements into the "bal" array

--- index.js
+++ index.js
@@ -20,8 +20,30 @@ mousey.addEventListener("mouseover", (event) => {
 const grav = 0.99;
 c.lineWidth = 5;
 let randomColor = () => {
-    return("rgba(Math.round(Math.random() * 250) + "," + 
-                Math.round(Math.random() * 250) + "," + 
-                Math.round(Math.random() * 250) + "," + 
-                Math.ceiling(Math.random() * 10)/ 10)")
+    return "rgb(" + Math.round(Math.random() * 250) + ',' + 
+                    Math.round(Math.random() * 250) + ',' + 
+                    Math.round(Math.random() * 250) + ',' + 
+                    Math.ceiling(Math.random() * 10) / 10 + ")"
+}
+
+const Ball = () => {
+    this.color = randomColor();
+    this.radius = Math.random() * 20 + 14;
+    this.startRadius = this.radius;
+    this.x = Math.random() * (tx - this.radius * 2) + this.radius;
+    this.y = Math.random() * (ty - this.radius);
+    this.dy = Math.random() * 2;
+    this.dx = Math.round((Math.random() - 0.5) * 10);
+    this.vel = Math.random() / 5;
+    this.update = () => {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        c.fillStyle = this.color;
+        c.fill();
+    };
+}
+
+let bal = [];
+for (let i = 0; i < 50; i++) {
+    bal.push(new Ball());
 }
